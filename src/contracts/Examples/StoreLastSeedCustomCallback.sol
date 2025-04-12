d9dc220fec5a9846d1912e2b2394a5718914c16e
Jakub Niewczas <niewczas.jakub@gmail.com>
2025-04-12 10:54:26 +0900

    add StoreLastSeedCustomCallback example

---
+++ src/contracts/Examples/StoreLastSeedCustomCallback.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import "../Easyntropy/EasyntropyConsumer.sol";
+
+contract StoreLastSeedCustomCallback is EasyntropyConsumer {
+  //
+  // support
+  bytes32 public latestSeed;
+
+  //
+  // events & errors
+  event RandomValueRequested(uint64 indexed requestId);
+  event RandomValueObtained(uint64 indexed requestId, bytes32 seed);
+  error NotEnoughEth();
+
+  constructor(address _entropy) EasyntropyConsumer(_entropy) {}
+
+  //
+  // --- entropy usage demo: custom fulfill callback --------------------------
+  function requestRandomValueCustomCallback() public payable returns (uint64 requestId) {
+    if (msg.value < easyntropyFee()) revert NotEnoughEth();
+
+    requestId = easyntropyRequestWithCallback(this.customFulfill.selector);
+
+    emit RandomValueRequested(requestId);
+  }
+
+  function customFulfill(uint64 requestId, bytes32 seed) external onlyEasyntropy {
+    latestSeed = seed;
+
+    emit RandomValueObtained(requestId, seed);
+  }
+
+  //
+  // --- optional calculateSeed customisation, simplified for tests -------------
+  function calculateSeed(bytes32 externalSeed) internal pure override returns (bytes32 result) {
+    result = externalSeed;
+  }
+  // ----------------------------------------------------------------------------
+
+  //
+  // money managment
+  receive() external payable {}
+}
