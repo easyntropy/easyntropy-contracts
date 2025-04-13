fd43d6fe1d05cf4910f9be2cc460d073903179f8
Jakub Niewczas <niewczas.jakub@gmail.com>
2025-04-13 13:41:46 +0900

    unify dev env with other projects

--- scripts/run-dev-env-docker.sh
+++ scripts/run-dev-env-docker.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
 
-COMPOSE_MENU=0 docker compose -f ./scripts/support/dev-env-docker-compose.yml up --build
-COMPOSE_MENU=0 docker compose -f ./scripts/support/dev-env-docker-compose.yml down
+COMMAND=$1
+shift
+ARGS="$@"
+
+
+case "$COMMAND" in
+  shell)
+    if [ -z "$ARGS" ]; then
+      COMPOSE_MENU=0 docker compose -f ./scripts/support/dev-env-docker-compose.yml run $([ -z "$NO_PORT" ] && echo '-p 3000:3000 -p 3001:3001') --remove-orphans shell bash
+    else
+      COMPOSE_MENU=0 docker compose -f ./scripts/support/dev-env-docker-compose.yml run $([ -z "$NO_PORT" ] && echo '-p 3000:3000 -p 3001:3001') --remove-orphans shell "$ARGS"
+    fi
+    ;;
+
+  support)
+    COMPOSE_MENU=0 docker compose -f ./scripts/support/dev-env-docker-compose.yml up --build anvil
+    COMPOSE_MENU=0 docker compose -f ./scripts/support/dev-env-docker-compose.yml down
+    ;;
+
+  *)
+    echo "Usage: $0 {shell|support}"
+    ;;
+esac
